8454936fbec87d0b57b93486e357694f0fdfef0f
“wuxxuu” <3373325472@qq.com>
2022-09-23 08:19:08 +0800

    test

--- login_register/login_register/urls.py
+++ login_register/login_register/urls.py
@@ -17,11 +17,8 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),      test!!!!!!!!!!
+    path("admin/", admin.site.urls),
 ]
 
 
-This is test!!!!
 
-
-test branck again!!!
